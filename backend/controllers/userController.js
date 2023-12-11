71b8dc2b4f6ea6613949954854452c2636b37b8f
removethejuice <diegojesuschavezbotto@gmail.com>
2023-12-11 01:42:16 -0600

    avance en los controladores de los demas metodos

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,22 @@
+//basicamente esta es la carne de los metodos y lo exportamos a userRoutes
 const getUsers = (req,res) => {
     res.status(200).json({message:'Get users'})
 }
 
+const postUsers = (req,res) => {
+    res.status(200).json({message:'Post users'})
+}
+
+const putUsers = (req,res) => {
+    res.status(200).json({message:'Put users'})
+}
+
+
+const deleteUsers = (req,res) => {
+    res.status(200).json({message:'Delete users'})
+}
+
+
 module.exports = {
-    getUsers,
+    getUsers,putUsers,postUsers,deleteUsers
 }
