b78fc03ae5ac12257339e405b69da67f5ae16b4f
removethejuice <diegojesuschavezbotto@gmail.com>
2023-12-14 01:17:54 -0600

    arregle otro error masivo una coma en el middleware me quito una hora de mi vida

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-// Importando clases y funciones
+// Importando clases y funciones que utilizaremos
 const {
   User,
   Property,
@@ -8,6 +8,7 @@ const {
   ReservationStatus,
   PaymentOptions } = require('../Classes.js');
 
+  // esto es para conectar la base de datos no se exactamente que hace pero sirve para la base de datos
 const {
   pingdb,
   connectToMongoDB,
@@ -15,7 +16,7 @@ const {
 
 const { initiateFirebase } = require('../configfirebase.js');
 
-// Importando express y el uri y demas
+// Importando express e inicializando el appinstance
 
 const express = require('express');
 const colors = require('colors')
@@ -25,22 +26,28 @@ const connectDB = require('../backend/config/db.js')
 const appInstance = express();
 
 // Importing 'cors' middleware for handling Cross-Origin Resource Sharing.
+
 const cors = require('cors');
 appInstance.use(cors());
+
 
 // Requerimientos para iniciar el client
 const bodyParser = require('body-parser');
 const { initializeApp } = require('firebase/app');
 const { MongoClient } = require('mongodb');
 
-// esto es para que sepa como desencodear los url
+// esto es para que sepa como desencodear los url para parsear sin esto el URI no funciona para nada y todos los URL tampoco esto es vital
+
 const urlEncodeParser = bodyParser.urlencoded({ extended: true });
 appInstance.use(urlEncodeParser);
 
-// Seteando el port, mirar el archivo .env que esta en la root del documento para mas detalles
+// Seteando el port, mirar el archivo .env que esta en la root del documento para mas detalles basicamente aca digo que uso el port 5000
+
 const port = process.env.PORT || 5000;
 connectDB()
+
 // Inicializando el servidor
+
 appInstance.listen(port, async () => {
   console.log("Server is running and listening on port", port);
   await pingdb(console.dir);
@@ -49,33 +56,10 @@ appInstance.listen(port, async () => {
 
 //esta linea conecta a el CRUD con user Routes que contiene todos los metodos para los users
 appInstance.use('/api/users', require('../routes/userRoutes.js'))
+// esta linea de abajo es el middleware basicamente hace que los errores se envien de manera personalizada con mas info y no en css si no que en json, para mas info revisar el file errorMiddleware.js 
 appInstance.use(errorHandler);
 
-appInstance.post('/createuser', async (req, res) => {
-  let client
-  try {
-    // nos conectamos a la base de datos proyectoUX y a la coleccion clientes
-     client = new MongoClient(process.env.MONGO_URI);
-    const database = client.db("proyectoUX");
-    const clientes = database.collection("clientes");
-   
-    const doc = req.body
 
-    // Insert the defined document into the "clientes" collection
-    const result = await clientes.insertOne(doc);
-    // Print the ID of the inserted document
-    console.log(`A document was inserted with the _id: ${result.insertedId}`);
-  } 
-catch (error) {
-  console.error('Error:', error);
-  res.status(500).json({ error: 'Internal Server Error', details: error.message });
-}
-  finally {
-    // Close the MongoDB client connection
-    res.status(200).json({message:'Todo salio bien rick'})
-    await client.close();
-  }
-})
 
 
 
